65f1713fe8eca882ddc03a3bbdd6ddc130bebd55
Nick Fallon <nickfallonwastaken@gmail.com>
2017-08-02 21:59:09 -0600

    Components are displaying

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Splash from '@/components/Splash'
+import About from '@/components/About'
 import Projects from '@/components/Projects'
+import Contact from '@/components/Contact'
 
 Vue.use(Router)
 
@@ -16,6 +18,16 @@ export default new Router({
       path: '/projects',
       name: 'Projects',
       component: Projects
+    },
+    {
+      path: '/about-me',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
     }
   ]
 })
